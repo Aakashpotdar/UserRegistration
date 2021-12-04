e054233140889ad1cc5a6d5dfe3beee7e630b859
aakashpotdar <potdaraakash@gmail.com>
2021-12-04 17:15:42 +0530

    Costomized exception is added

--- UserRegistrationAndJUnit/isValid.cs
+++ UserRegistrationAndJUnit/isValid.cs
@@ -7,39 +7,71 @@ namespace UserRegistrationAndJUnit
 {
     public class isValid
     {
-
         public static bool CheckFirstName(string name)
         {
             string regexName = "^([A-Z][a-z]{3}[a-z]+)$";
-            return Regex.IsMatch(name, regexName);
-           
-            
+            bool check = Regex.IsMatch(name, regexName);
+            if (check)
+            {
+                return true;
+            }
+            else
+            {
+                throw new UserRegistrationFirstNameExcption("invalid First name" + name);
+            }
         }
         public static bool CheckLastName(string name)
         {
             string regexLastName = "^([A-Z][a-z]{3}[a-z]+)$";
-            return Regex.IsMatch(name, regexLastName);
-            
-           
+            bool check = Regex.IsMatch(name, regexLastName);
+            if (check)
+            {
+                return true;
+            }
+            else
+            {
+                throw new UserRegistrationLastNameExcption("invalid Last name" + name);
+            }
         }
         public static bool CheckMailId(string Id)
         {
             string regexId = "^([a-zA-Z0-9]+)(.?)([a-zA-Z0-9]+)([@][a-z]+)([.][a-z]+)$";
-           return Regex.IsMatch(Id, regexId);
+            bool check = Regex.IsMatch(Id, regexId);
+            if (check)
+            {
+                return true;
+            }
+            else
+            {
+                throw new UserRegistrationMailIdExcption("invalid MailId" + Id);
+            }
         }
         public static bool CheckMobileNumber(string Number)
         {
             string regexNumber = "^[9][1][-][6-9][0-9]{9}$";
-           return  Regex.IsMatch(Number, regexNumber);
-            
+            bool check = Regex.IsMatch(Number, regexNumber);
+            if (check)
+            {
+                return true;
+            }
+            else
+            {
+                throw new UserRegistrationFirstNameExcption("invalid number" + Number);
+            }
         }
 
         public static bool CheckPassword(string Password)
         {
             string regexPassword = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[@#$^&*])(?=.*?[0-9]).{8,}$";
-            return Regex.IsMatch(Password, regexPassword);
-
-            
+            bool check = Regex.IsMatch(Password, regexPassword);
+            if (check)
+            {
+                return true;
+            }
+            else
+            {
+                throw new UserRegistrationFirstNameExcption("invalid Password" + Password);
+            }
         }
     }
 }
